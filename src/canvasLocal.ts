94bb7f955a71a7e2022d7cdcdd681394a809132f
UrielMorales <urielmorales@gmail.com>
2021-04-07 19:18:15 -0500

    Malla Heptagono

--- src/canvasLocal.ts
+++ src/canvasLocal.ts
@@ -32,70 +32,59 @@ export class CanvasLocal {
     this.graphics.stroke();
   }
 
-  paint() {
-    //this.graphics.arc(this.iX(0), this.iY(0), Math.abs(this.iX(4)-this.iX(0)), 0,2*Math.PI, false);
-    //this.graphics.stroke();
-    //this.graphics.fillText("Lienzo listo desde ts", this.iX(2), this.iY(3.7));
-    //debugger;
-    //this.drawLine(320, 40, 480, 400);
-    //this.drawLine(320, 40, 140, 400);
-    //this.drawLine(140, 400, 480, 400);
-    let lado = 100;
+    paint_Heptagono() {
+    let lado = 200;
     let side = 0.95 * lado;
     let sideHalf = 0.5 * side;
     let xCenter = 320;
     let yCenter = 240;
-      
     let h = sideHalf * Math.sqrt(3);
-    let xA, yA, xB, yB, xC, yC,
-    xA1, yA1, xB1, yB1, xC1, yC1, p, q;
-     q = 0.05;
+    let xA, yA, xB, yB, xC, yC,xD, yD, xE , yE, xF , yF, xG , yG,
+        xA1, yA1, xB1, yB1, xC1, yC1, xD1, yD1, xE1 , yE1, xF1 , yF1, xG1 , yG1, p, q;
+    q = 0.05;
     p = 1 - q;
-    /*xA = xCenter - sideHalf;
-    yA = yCenter - 0.5 * h;
-    xB = xCenter + sideHalf;
-    yB = yA;
-    xC = xCenter;
-    yC = yCenter + 0.5 * h; */
-
-    for (let m = 0; m < 4; m++){
-      for (let n = 0; n < 4; n++){
-        xA = 100+n*lado - sideHalf;
-        yA = 100+m*lado - 0.5 * h;
-        xB = 100+n*lado+ sideHalf;
-        yB = yA;
-        xC = 100+n*lado;
-        yC = 100+m*lado + 0.5 * h; 
-        for (let i = 0; i < 20; i++){
+    for (let m = 0; m < 3; m++){
+      for (let n = 0; n < 3; n++){
+        xA = 100+n*lado - sideHalf +75
+        yA = 100+m*lado - 0.5 * h + 50  ;
+        xB = 100+n*lado + sideHalf -35 ;
+        yB = 100+m*lado - 0.5 * h ;
+        xC = 100+n*lado + sideHalf +34;
+        yC = 100+m*lado + 0.5 * h - 115;
+        xD = 100+n*lado - sideHalf +235;
+        yD = 100 + m * lado + 0.5 * h - 50;
+        xE = 100+n*lado + sideHalf ;
+        yE = 100 + m * lado + 0.5 * h + 10;
+        xF = 100+n*lado + sideHalf  - 90;
+        yF = 100 + m * lado + 0.5 * h +10;
+        xG = 100+n*lado + sideHalf  - 130;
+        yG = 100 + m * lado + 0.5 * h - 50;
+        for (let i = 0; i < 50; i++){
           this.drawLine(xA, yA, xB, yB);
           this.drawLine(xB, yB, xC, yC);
-          this.drawLine(xC, yC, xA, yA);
+          this.drawLine(xC, yC, xD, yD);
+          this.drawLine(xD, yD, xE, yE);
+          this.drawLine(xE, yE, xF, yF);
+          this.drawLine(xF, yF, xG, yG);
+          this.drawLine(xG, yG, xA, yA);
           xA1 = p * xA + q * xB;
           yA1 = p * yA + q * yB;
           xB1 = p * xB + q * xC;
           yB1 = p * yB + q * yC;
-          xC1 = p * xC + q * xA;
-          yC1 = p * yC + q * yA;
-          xA = xA1; xB = xB1; xC = xC1;
-          yA = yA1; yB = yB1; yC = yC1;
+          xC1 = p * xC + q * xD;
+          yC1 = p * yC + q * yD;
+          xD1 = p * xD + q * xE;
+          yD1 = p * yD + q * yE;
+          xE1 = p * xE + q * xF;
+          yE1 = p * yE + q * yF;
+          xF1 = p * xF + q * xG;
+          yF1 = p * yF + q * yG;
+          xG1 = p * xG + q * xA;
+          yG1 = p * yG + q * yA;
+          xA = xA1; xB = xB1; xC = xC1; xD = xD1; xE = xE1;xF = xF1;xG = xG1;
+          yA = yA1; yB = yB1; yC = yC1; yD = yD1; yE = yE1; yF = yF1; yG = yG1;
         }
       }
     }
-
-    /* for (let i = 0; i < 50; i++){
-        this.drawLine(xA, yA, xB, yB);
-        this.drawLine(xB, yB, xC, yC);
-        this.drawLine(xC, yC, xA, yA);
-        xA1 = p * xA + q * xB;
-        yA1 = p * yA + q * yB;
-        xB1 = p * xB + q * xC;
-        yB1 = p * yB + q * yC;
-        xC1 = p * xC + q * xA;
-        yC1 = p * yC + q * yA;
-        xA = xA1; xB = xB1; xC = xC1;
-        yA = yA1; yB = yB1; yC = yC1;
-    } */
-    
   }
-
 }
